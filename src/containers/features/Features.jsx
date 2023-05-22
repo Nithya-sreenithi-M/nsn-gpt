1c6395a4308e81374d91c9d4611b5e5251674def
unknown <nithyasreenithi7@gmail.com>
2023-05-22 20:51:53 +0530

    components/feature.css and .jsx is completed

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,11 +4,10 @@ import React from "react";
 //give style to the component
 import './features.css'
 const Features = () =>{
-    return(<div>
-        Features
-    
-       
-        </div>)
+    return(
+        
+        <div>
+        </div>
+    )
 }
-
 export default Features;
--- src/containers/whatgpt/Whatgpt.jsx
+++ src/containers/whatgpt/Whatgpt.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 //give style to the component
 import './whatgpt.css'
-import Features from '../features/Features'
+import Feature from '../../components/feature/Feature'
 const Whatgpt  = () =>{
     return(
         <div className="gpt3__whatgpt3 section__margin" id="whgpt3">
@@ -11,18 +11,18 @@ const Whatgpt  = () =>{
                 {/*what is GPT-3, chatbots, knowledgebase and education has same kind of feature.
                     Lets define them under feature component and reuse them.
             */}
-            <Features/>
+            <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
             </div>
             <div className="gpt3__whatgpt3-heading">
-                <h1 className="gradient-text">
+                <h1 className="gradient__text">
                 The possibilities are beyond your imagination
                 </h1>
                 <p>Explore The Library</p>
             </div>
             <div className="gpt3__whatgpt3-container">
-                <Features/>
-                <Features/>
-                <Features/>
+                <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
+                <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
+                <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"/>
             </div>
         </div>
     )
