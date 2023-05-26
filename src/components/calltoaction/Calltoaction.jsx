6f3b49338392794ce53fb4d7e67ae2478dfc4b09
unknown <nithyasreenithi7@gmail.com>
2023-05-26 21:36:22 +0530

    CTA  section is completed

--- src/components/calltoaction/Calltoaction.jsx
+++ src/components/calltoaction/Calltoaction.jsx
@@ -4,11 +4,20 @@ import React from "react";
 //give style to the component
 import './calltoaction.css'
 const Calltoaction = () =>{
-    return(<div>
-        Calltoaction
-    
+    return(
+        <div className="gpt3__cta">
+        <div className="gpt3__cta-content">
+            <p> Request Early Access to Get Started</p>
+            <h3>Register today & start exploring the endless possiblities.</h3>
+        </div>
+        <div className="gpt3__cta-btn">
+            <button type="button"> Get Started</button>
+        </div>
+
+
        
-        </div>)
+        </div>
+    )
 }
 
 export default Calltoaction;
