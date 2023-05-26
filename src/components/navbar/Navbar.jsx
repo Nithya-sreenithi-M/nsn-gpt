7610c0be73377bb8665bc7eb9c00630babc8a307
unknown <nithyasreenithi7@gmail.com>
2023-05-26 18:43:19 +0530

    possibility section is ove

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () =>{
                 <div className="gpt3__navbar-links_container">
                     <p><a href="#home">Home</a></p>
                     <p><a href="#whgpt3">What is GPT</a></p>
-                    <p><a href="#home">Open AI</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
                     <p><a href="#home">Case studies</a></p>
                     <p><a href="#home">Library</a></p>
                 </div> 
